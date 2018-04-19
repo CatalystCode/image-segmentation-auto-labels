266de20004274475b298799f51851c3f6b8213aa
Clemens Wolff <clewolff@microsoft.com>
2018-04-19 14:20:46 -0400

    Remove test docker dependency

--- .travis/ci.py
+++ .travis/ci.py
@@ -1,51 +1,33 @@
 #!/usr/bin/env python3
 
-from ast import literal_eval
-from getpass import getuser
-from os import getenv
 from os.path import abspath
 from os.path import dirname
 from os.path import join
 from os.path import realpath
-from subprocess import check_output
-from sys import stdout
+from sys import path
 from unittest import TestCase
 
+root_dir = abspath(join(dirname(realpath(__file__)), '..'))
+path.append(join(root_dir, 'src'))
 
-DOCKER_CONTEXT = abspath(join(dirname(realpath(__file__)), '..'))
-DOCKER_USERNAME = getenv('DOCKER_USERNAME', getuser())
-DOCKER_IMAGE_NAME = 'image_segmentation_auto_labels'
-DOCKER_TAG = '{}/{}:latest'.format(DOCKER_USERNAME, DOCKER_IMAGE_NAME)
-DEFAULT_ALGORITHM = 'otsu_hue'
-
-
-def run_command(args):
-    return check_output(args, shell=True).decode(stdout.encoding)
-
-
-def docker_do(args):
-    return run_command(['docker', 'run', DOCKER_TAG, '/do'] + args)
+import labelling  # noqa
 
 
 class EndToEndTest(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        run_command(['docker', 'build', '--tag', DOCKER_TAG, DOCKER_CONTEXT])
+    algorithm = 'otsu_hue'
 
     def test_list_algorithms(self):
-        algorithms = docker_do(['list_algorithms'])
-        algorithms = literal_eval(algorithms)
+        algorithms = labelling.list_algorithms()
 
         self.assertGreater(len(algorithms), 0)
-        self.assertIn(DEFAULT_ALGORITHM, algorithms)
+        self.assertIn(self.algorithm, algorithms)
 
     def test_create_mask(self):
-        image = '/data/test_image.jpg'
-        algorithm = DEFAULT_ALGORITHM
-        morph = '0'
+        image = join(root_dir, 'test_image.jpg')
+        algorithm = labelling.image_mask_algorithm_type(self.algorithm)
+        morph = 0
 
-        mask = docker_do(['create_mask', image, algorithm, morph])
-        mask = literal_eval(mask)
+        mask = labelling.create_mask(image, algorithm, morph)
 
         self.assertIn('content', mask)
         self.assertIn('type', mask)
--- src/constants.py
+++ src/constants.py
@@ -2,5 +2,5 @@ from ast import literal_eval
 from os import getenv
 
 
-MASK_COLOR = literal_eval(getenv('MASK_COLOR'))
-OUTPUT_IMAGE_FORMAT = getenv('OUTPUT_IMAGE_FORMAT')
+MASK_COLOR = literal_eval(getenv('MASK_COLOR', '(0, 0, 255)'))
+OUTPUT_IMAGE_FORMAT = getenv('OUTPUT_IMAGE_FORMAT', 'jpg')
